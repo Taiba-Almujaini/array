5719089d1986e5419e5ed64292c1dbf3793a6cd2
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-02-20 11:48:58 +0400

    Return an int array length 3 containing the first 3 digits of pi

---
+++ makePi.java
@@ -0,0 +1,15 @@
+
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] piArr = {3, 1, 4};
+        System.out.println(java.util.Arrays.toString(piArr)); 
+
+    }
+}
